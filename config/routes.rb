3211af5668c0fd13fc29b76cd40adb5b64d19cd8
mamallama <reneeq25@gmail.com>
2015-10-05 11:08:56 -0400

    migrations

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#form'
+  get 'home/form'
   post 'home/form'
 
   # Example of regular route:
